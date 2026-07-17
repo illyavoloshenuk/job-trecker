69cda61056ca20786cf1c49eefa0c64410d4961e
illyaoloshenuk <illyavolosenuk@gmail.com>
2026-07-17 21:28:26 +0200

    feat(users): add CRUD views for user profile

--- users/views.py
+++ users/views.py
@@ -49,19 +49,49 @@ def user_home(request):
         {'error': 'Method not allowed'},
         status=405,
     )
-
+@csrf_exempt
 def user_detail(request, id):
     try:
         user = UserProfile.objects.get(id=id)
-        data = {
-
-            'id': user.id,
-            'name': user.name,
-            'email': user.email,
-            }
-        return JsonResponse(data)
     except UserProfile.DoesNotExist:
         return JsonResponse(
             {'error': 'User not found'},
             status=404,
-)
+        )
+    if request.method == 'GET':
+        data ={
+            'id': user.id,
+            'name': user.name,
+            'email':user.email,
+        }
+        return JsonResponse(data)
+
+    if request.method == 'PATCH':
+        body = json.loads(request.body)
+
+        if 'name' in body:
+            user.name = body['name']
+
+        if 'email' in body:
+            user.email = body['email']
+
+        user.save()
+
+        return JsonResponse(
+            {
+                'id': user.id,
+                'name': user.name,
+                'email': user.email,
+            }
+        )
+
+    if request.method == 'DELETE':
+        user.delete()
+        return JsonResponse(
+            {},
+            status=204,
+        )
+    return JsonResponse(
+        {'error': 'Method not allowed'},
+        status=405,
+    )
